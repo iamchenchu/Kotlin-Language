b1bd3da51f8130aa2e6bb0f7f01bf92a562767a9
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-18 07:23:59 -0600

    adding the first code for kotlin

--- 1Basic.kt
+++
@@ -1,8 +0,0 @@
-//this is the first program in kotlin language 
-//All file names should be saved as .kt 
-//this is very much compatable with the java and we can use the java in kotlin also 
-
-
-fun main(){
-    println("Welcome to the world of kotlin programming")
-}
