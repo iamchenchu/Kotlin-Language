78434a3ecdbb575b4e68b234f60edd8cf835af0d
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-21 06:59:48 -0600

    added file for the syntax of the kotlin

---
+++ 1Basics/2Syntax.kt
@@ -0,0 +1,7 @@
+// fun is the keyword used to declare the function 
+//Before Kotlin version 1.3, it was required to use the main() function with parameters, like: fun main(args : Array<String>). The example above had to be written like this to work:
+
+fun main()
+{
+    println("Hello World")
+}
