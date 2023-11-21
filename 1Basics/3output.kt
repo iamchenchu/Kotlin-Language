a9b6431f5ca6f15626d88fd42add190474e2f51b
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-21 07:15:32 -0600

    Added the code files along with the explanation

---
+++ 1Basics/3output.kt
@@ -0,0 +1,16 @@
+// the println() function is used to output values or text
+// we can also use the print() function but the only difference is that it won't start with the new line of output everytime
+
+fun main()
+{
+    println("Hey chenchu")    
+    print(" Welcome to the kotlin programming")
+    println("this is easy to learn. ")
+    print("easy to understand")
+}
+
+// output:
+
+// Hey chenchu
+//  Welcome to the kotlin programmingthis is easy to learn. 
+// easy to understand%  
---
+++ 1Basics/4variables.kt
@@ -0,0 +1,22 @@
+// variables are containers  for storing the data
+// To create variables we use var and val and assign the value to it using the eaqual(=) sign
+// var variablename = value
+// val variablename = value
+
+// the difference between var and val is that the variables declared with var keyword can be changed
+// and modified where as val variables can not be changed once declared
+// we not need to specify the data type in the kotlin language
+
+fun main() {
+    val fname = "Chenchu"
+    var lname = "Reddy"
+    val birthyear = 1998
+
+    println(fname + " "+lname)
+    println("I am born in the year of : "+ birthyear)
+    lname = "Naidu"   // changed the lname as we can change the var variable
+    
+    println(fname + " "+lname)
+
+
+}
