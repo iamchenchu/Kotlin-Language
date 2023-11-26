f1ae6166888ba8b685ea470458edb7d0dd3315e3
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-25 23:48:49 -0600

    Added the code for Arithmetic operators in kotlin

---
+++ 1Basics/6Operators.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    var x =100
+    val y = 10
+    var c = x * y
+    var d = x + y
+    var e = x - y
+    var f = x / y
+
+    println(c + d + e + f)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    
+
+    
+}
